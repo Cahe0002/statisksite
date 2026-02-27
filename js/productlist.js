196133716bed28401c4e950ece62aa4fe61d9002
Caroline S <cahe0002@stud.ek.dk>
2026-02-27 09:50:24 +0100

    added js to productlist

--- js/productlist.js
+++ js/productlist.js
@@ -1,29 +1,37 @@
-const endpoint = "https://kea-alt-del.dk/t7/api/products/";
-
 const container = document.querySelector("#productlist");
+const endpoint = `https://kea-alt-del.dk/t7/api/products`;
 
 function getData() {
   fetch(endpoint)
-    .then((respons) => respons.json())
+    .then((res) => res.json())
     .then(showData);
 }
 
 function showData(data) {
+  let markup = "";
   console.log(data);
   data.forEach((product) => {
-    container.innerHTML += `
-    <a class=product-container href="product.html">
-    <article class="cardproduct soldout">
-    <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="puma bag"/>
-    <span class="sold">${product.soldout}</span>
-    <h3>Grey Puma Backpack${product.productdisplayname}</h3>
-    <p class="product">${product.articletype} | ${product.brandname}</p>
-    <p>DKK <span class="line">${product.price}</span>,-</p>
-    <p>Now DKK 795,-></p>
-    <p class="sale">${product.discount}</p>
-    </article>
-    </a>`;
+    console.log(product);
+    markup += `
+    <a href="product.html">
+          <article class="cardproduct soldout">
+            <img
+              src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"
+              alt="puma bag"
+            />
+            <span class="sold">Sold Out</span>
+            <h3>${product.productdisplayname}</h3>
+            <p class="product">Backback | Puma</p>
+            <p>DKK <span class="line">${product.price}</span>,-</p>
+            <div class="discount">
+              <p>Now DKK 795,-</p>
+              <p class="sale">25%</p>
+            </div>
+          </article>
+        </a>
+        `;
   });
+  container.innerHTML = markup;
 }
 
 getData();
