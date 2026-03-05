c1de780fa49fbf2e6aebd4652cddec9a6ef4d3e1
Caroline S <cahe0002@stud.ek.dk>
2026-03-05 08:36:04 +0100

    test new

--- js/productdetails.js
+++ js/productdetails.js
@@ -14,13 +14,12 @@ function showData(details) {
   console.log(details);
   container.innerHTML = `<a href="productlist.html?category=${details.category}">
         <h3>Back</h3>
-      </a> <section class="product-img-text" ${details.soldout && "soldout"}">
+      </a> <section class="product-img-text">
         <div>
           <img
             src="https://kea-alt-del.dk/t7/images/webp/640/${details.id}.webp"
             alt="${details.productdisplayname}"
           />
-          <span class="sold">Sold Out</span>
         </div>
         <div class="product-text">
           <h2>${details.productdisplayname}</h2>
@@ -28,6 +27,7 @@ function showData(details) {
           <p>Category: ${details.category}</p>
           <p>Brandname: ${details.brandname}</p>
           <p>Price: ${details.price},-</p>
+          ${details.discount ? `<p>Discount:${details.discount} ,-</p>` : ""}
           <button>Buy now</button>
         </div>
       </section>`;
